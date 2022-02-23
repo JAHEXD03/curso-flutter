b67651ab11a5423d0ec4415cc6a7ab74cf95fb68
JAHEXD03 <arturohinojosa@micorreo.upp.edu.mx>
2022-02-23 15:51:27 -0600

    Variables e tipo List

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,25 @@
+void main(List<String> arguments) {
+  /* // VARIABLES DE TIPO LISTA
+
+  //lista de valores de tipo String
+  List<String> coches = ["car1", "car2", "car3"];
+
+  //el metodo forEach nos ayuda a recorrer la lista e asignar un indice a los elemntos
+  //y esto se imprime
+  coches
+      // ignore: avoid_function_literals_in_foreach_calls
+      .forEach((auto) => print("indice: ${coches.indexOf(auto)} valor $auto"));
+
+  // se imprime el tamaño de la lista
+  int lisSize = coches.length;
+  print("$lisSize");
+
+  //se sabe si se encutra un elemento en la lista o no
+  bool respueta = coches.contains("car9");
+  print("¿ Se encuetra el objeto? $respueta");
+
+  //se crea un objeto e tipo Map el cual ns ayuda a asignar una key a cada valor
+  Map map = {1: "one", 2: "two", 3: "three"};
+
+  print(map[1]);*/
+}
